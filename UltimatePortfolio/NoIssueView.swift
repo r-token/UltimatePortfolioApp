20217fee2561c030da8f24ed736805cfc5dfabfb
Ryan Token <ryantoken13@gmail.com>
2023-11-04 12:40:14 -0600

    Add issues and tags; rename tags

--- UltimatePortfolio/NoIssueView.swift
+++ UltimatePortfolio/NoIssueView.swift
@@ -15,9 +15,7 @@ struct NoIssueView: View {
             .font(.title)
             .foregroundStyle(.secondary)
         
-        Button("New Issue") {
-            print("Making new issue")
-        }
+        Button("New Issue", action: dataController.newIssue)
     }
 }
 
